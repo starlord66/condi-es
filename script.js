9049732577164519b2ac580c4258cf623f9467e2
Dell Schulz <dells@DESKTOP-P01LTV4.localdomain>
2022-07-01 10:49:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let nome = "Dell"
+console.log(nome)
+
+let number = 27
+console.log(number)
+
+let year = 2022
+let menos = year - number
+console.log(menos)
+
+console.log(year + number)
+
+console.log("estou mexendo no console log")
+
+let quartaFeira = true
+console.log(quartaFeira)
+
+console.log(typeof quartaFeira)
